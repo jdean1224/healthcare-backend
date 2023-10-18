c17194c9c88ce03754ffff7ae6411227e8c26ab4
Justin Koski <jdeankoski@gmail.com>
2023-10-17 21:48:45 -0400

    Fix bub

--- controllers/authController.js
+++ controllers/authController.js
@@ -30,7 +30,7 @@ exports.login = catchAsync(async (req, res, next) => {
   const user = await User.findOne({ email }).select('+password');
 
   if (!user || !(await user.correctPassword(password, user.password))) {
-    console.log('Oncorrect email or password!');
+    console.log('Error logging in!');
   }
 
   const token = signToken(user._id);
--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -16,7 +16,7 @@ class APIFeatures {
   }
   sort() {
     if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.spli(',').join(' ');
+      const sortBy = this.queryString.sort.split(',').join(' ');
       this.query = this.query.sort(sortBy);
     } else {
       this.query = this.query.sort('-createdAt');
